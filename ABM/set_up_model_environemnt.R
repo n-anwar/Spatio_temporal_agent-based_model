4b4fbf2f471abc757ac323c5c1ab9d257666fd26
n-anwar <n.anwarju@gmail.com>
2025-05-15 15:48:20 +1000

    Update

--- ABM/set_up_model_environemnt.R
+++ ABM/set_up_model_environemnt.R
@@ -1,4 +1,6 @@
 
+source('~/ABM/model_parameters.R')
+
 library('raster')
 library(tidyverse)
 library(ggpubr)
@@ -189,4 +191,30 @@ for(ii in 1:length(hh_labels)){
     
   }
   
+}
+
+
+
+
+mean_seasonality<-rowMeans(seasonal_MAP_hh)
+scaled_seasonal_MAP <-seas_param_a*(seasonal_MAP_hh^(seas_param_alph))
+
+
+hh_bite_prop<-((1-bite_reduction_IRS)^(1*(hh_treatment==2 | hh_treatment==3)))*((1-bite_reduction_LLIN)^(1*(hh_treatment==4 | hh_treatment==3)))
+hh_deathonbite<-1-((1-death_IRS)^(1*(hh_treatment==2 | hh_treatment==3)))*((1-death_LLIN)^(1*(hh_treatment==4 | hh_treatment==3)))
+propto_mozzycreation <-(transHM_per_bite/transMH_per_bite)*hh_bite_prop*(1-hh_deathonbite)/N
+seasonal_creation<-matrix(0,nrow=sum(N),ncol=12) 
+
+for(ii in 1:length(hh_labels)){
+  for(jj in 1:N[ii]){
+    #specifies the row of the data set
+    if(ii!=1){
+      row_num<-sum(N[1:(ii-1)])+jj
+    }else{
+      row_num<-jj
+    }
+    hh_row_indicies<-which(hh_labels==ind_level_data$hh_id[row_num])
+    seasonal_creation[row_num,] <- scaled_seasonal_MAP[hh_row_indicies,]*propto_mozzycreation[hh_row_indicies]
+    
+  }
 }
